ea3b5a26729d713ccef80dbab7d9573198ea4c1d
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-08-28 16:16:06 -0300

    Adicionado loading.

--- js/script.js
+++ js/script.js
@@ -10,14 +10,15 @@ function cliquebotao(event){
         type: "GET",
         datatype: "json",
         url: "http://devmedianotesapi.azurewebsites.net/api/notes/"+id,
+        beforeSend: aguardaNota,
         success: exibenota,
-        error: $("#retorno").html("Api fora do ar.")
+        error: retornaErro
     });
 }
 
 function exibenota(data){
     if ($.isArray(data)) {
-        $("#retorno").html("");
+        $("#retorno").empty();
         $.each(data, function(index, value){
             $("#retorno").prepend("Id: " + value.id + "<br>Titulo: " + value.title + "<br>Conteudo: " + value.body + "<br>");
 
@@ -25,4 +26,14 @@ function exibenota(data){
     } else {
         $("#retorno").html("Id: " + Data.id + "<br>Titulo: " + data.title + "<br>Conteudo: " + data.body + "<br>");
     }
+    $("#loading").empty();
+}
+
+function aguardaNota(){
+    $("#loading").html("<img style='width: 15px; margin-left: 10px;' src='https://www.devmedia.com.br/cursos/img/loading.gif' alt='loading'>");
+}
+
+function retornaErro(){
+    $("#retorno").html("Api fora do ar.");
+    $("#loading").empty();
 }
